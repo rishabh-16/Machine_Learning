fe535a6aff7dfbc4cbe08e439b6f44ab6f5a18cb
rishabh-16 <akchitra99@gmail.com>
2018-12-15 18:40:29 +0530

    implemented using y=wx+c

--- Linear Regression using modules/Linear_Regression.py
+++ Linear Regression using modules/Linear_Regression.py
@@ -11,28 +11,36 @@ class  LReg:
     def __init__(self,X,y):
         m=len(y)
         n=np.shape(X)[1]
-        self.X=np.hstack((np.ones((m,1)),X))
+        self.X=X
         self.y=y
-        self.theta=np.zeros((n+1,1))
+        self.w=np.zeros((n,1))
+        self.c=[0]
         
-    def Calcost(self,theta):
+    def Calcost(self):
         m=len(self.y)
-        d=self.X.dot(theta)-self.y
+        d=np.dot(self.X,self.w)+self.c-self.y
         J=(1/(2.0*m)) * np.sum(np.square(d))
         return J
     
     def gradient_descent(self,alpha,noi):
         m=len(self.y)
         for i in range(noi):
-            d=self.X.dot(self.theta)-self.y
-            self.theta = self.theta - (alpha/m)*(self.X.T.dot(d))
-        return self.theta
+            pred=np.dot(self.X,self.w)+self.c
+            w_grad=self.X.T.dot(pred-self.y)
+            c_grad=np.sum(pred-self.y)
+            self.w-=(alpha/m)*w_grad
+            self.c-=(alpha/m)*c_grad
+        return self.w,self.c
     
     def predict(self,X):
-        m=np.shape(X)[0]
-        X=np.hstack((np.ones((m,1)),X))
-        y=X.dot(self.theta)
+        y=X.dot(self.w)+self.c
         return y
+    
+    def accuracy(self,y_pred,y_test):
+        error=(y_pred-y_test)/y_test *100
+        acc=100-np.mean(error)
+        return acc
+    
   #_________________________________________________________________________#  
 
 def normalize(X):
@@ -41,9 +49,5 @@ def normalize(X):
     Xn=(X-mean)/dev
     return Xn,mean,dev
 
-def accuracy(y_pred,y_test):
-    error=(y_pred-y_test)/y_test *100
-    acc=100-np.mean(error)
-    return acc
-    
+
 
--- Logistic Regression using modules/Logistic_Regression.py
+++ Logistic Regression using modules/Logistic_Regression.py
@@ -4,9 +4,6 @@
 # In[ ]:
 import numpy as np
 
-def sigmoid(z):
-    ans=1/(1+np.exp(-z))
-    return ans
 
 #_______________________________________________________________________________________#
 class  LogReg:
@@ -14,28 +11,40 @@ class  LogReg:
     def __init__(self,X,y):
         m=len(y)
         n=np.shape(X)[1]
-        self.X=np.hstack((np.ones((m,1)),X))
+        self.X=X
         self.y=y
-        self.theta=np.zeros((n+1,1))
+        self.w=np.zeros((n,1))
+        self.c=[0]
         
-    def Calcost(self,theta):
+    def sigmoid(self,z):
+        ans=1/(1+np.exp(-z))
+        return ans
+
+        
+    def Calcost(self):
         m=len(self.y)
-        pred=sigmoid(self.X.dot(theta))
+        pred=self.sigmoid(np.dot(self.X,self.w)+self.c)
         J=(1/m) * (-self.y.T.dot(np.log(pred))-(1-self.y).T.dot(np.log(1-pred)))
         return J
     
     def gradient_descent(self,alpha,noi):
         m=len(self.y)
         for i in range(noi):
-            pred=sigmoid(self.X.dot(self.theta))
-            self.theta = self.theta - (alpha/m)*(self.X.T.dot(pred-self.y))
-        return self.theta
+            pred=self.sigmoid(np.dot(self.X,self.w)+self.c)
+            w_grad=self.X.T.dot(pred-self.y)
+            c_grad=np.sum(pred-self.y)
+            self.w-=(alpha/m)*w_grad
+            self.c-=(alpha/m)*c_grad
+        return self.w,self.c
     
     def predict(self,X):
-        m=np.shape(X)[0]
-        X=np.hstack((np.ones((m,1)),X))
-        y=sigmoid(X.dot(self.theta))>=0.5
+        y=self.sigmoid(X.dot(self.w)+self.c)>=0.5
         return y.astype(int)
+    
+    def accuracy(self,y_pred,y_test):
+        A=(y_pred==y_test)
+        acc=np.mean(A)*100
+        return acc
 #________________________________________________________________________________________#
 
 def normalize(X):
@@ -44,10 +53,7 @@ def normalize(X):
     Xn=(X-mean)/dev
     return Xn,mean,dev
 
-def accuracy(y_pred,y_test):
-    A=(y_pred==y_test)
-    acc=np.mean(A)*100
-    return acc
+
     
 
 
